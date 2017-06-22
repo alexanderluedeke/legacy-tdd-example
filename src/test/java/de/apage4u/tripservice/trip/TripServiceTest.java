dd2fad48ce2a24206c9c50ea428358d597e35c0e
alexander.luedeke <alexander.luedeke@idealo.de>
2017-06-22 22:55:19 +0200

    4 Be more explicit by using constants.

--- src/test/java/de/apage4u/tripservice/trip/TripServiceTest.java
+++ src/test/java/de/apage4u/tripservice/trip/TripServiceTest.java
@@ -3,13 +3,28 @@ package de.apage4u.tripservice.trip;
 import org.junit.Test;
 
 import de.apage4u.tripservice.exception.UserNotLoggedInException;
+import de.apage4u.tripservice.user.User;
 
 public class TripServiceTest {
 
-	@Test(expected = UserNotLoggedInException.class)
-    public void should_throw_an_exception_when_user_is_not_logged_in() {
-        TripService tripService = new TripService();
+    private static final User GUEST = null;
+    private static final User UNUSED_USER = null;
 
-        tripService.getTripsByUser(null);
+    private User loggedInUser;
+
+    @Test(expected = UserNotLoggedInException.class)
+    public void should_throw_an_exception_when_user_is_not_logged_in() {
+        TripService tripService = new TestableTripService();
+        loggedInUser = GUEST;
+
+        tripService.getTripsByUser(UNUSED_USER);
+    }
+
+    private class TestableTripService extends TripService {
+
+        @Override
+        protected User getLoggedUser() {
+            return loggedInUser;
+        }
     }
 }
