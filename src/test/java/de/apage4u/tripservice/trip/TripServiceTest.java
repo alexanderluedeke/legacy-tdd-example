1d9a2377156203844d40a87d42236de92537a0c0
alexander.luedeke <alexander.luedeke@idealo.de>
2017-06-22 22:41:13 +0200

    2 Start testing from the smallest branch and learn something about the app.

--- src/test/java/de/apage4u/tripservice/trip/TripServiceTest.java
+++ src/test/java/de/apage4u/tripservice/trip/TripServiceTest.java
@@ -1,5 +1,15 @@
 package de.apage4u.tripservice.trip;
 
+import org.junit.Test;
+
+import de.apage4u.tripservice.exception.UserNotLoggedInException;
+
 public class TripServiceTest {
-	
+
+	@Test(expected = UserNotLoggedInException.class)
+    public void should_throw_an_exception_when_user_is_not_logged_in() {
+        TripService tripService = new TripService();
+
+        tripService.getTripsByUser(null);
+    }
 }
