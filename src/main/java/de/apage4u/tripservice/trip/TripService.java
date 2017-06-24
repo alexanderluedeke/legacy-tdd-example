40ab6da1f76066c11bc3bb1ba43c5f39004b781b
alexander.luedeke <alexander.luedeke@idealo.de>
2017-06-24 14:09:47 +0200

    13 Continue refactoring while the test are still green: Replace nested conditional (loggendIn) with guard clauses

--- src/main/java/de/apage4u/tripservice/trip/TripService.java
+++ src/main/java/de/apage4u/tripservice/trip/TripService.java
@@ -10,17 +10,17 @@ import de.apage4u.tripservice.user.UserSession;
 public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<Trip>();
         User loggedUser = getLoggedUser();
-
-        if (loggedUser != null) {
-            if (user.isFriendWith(loggedUser)) {
-                tripList = tripsByUser(user);
-            }
-            return tripList;
-        } else {
+        if (loggedUser == null) {
             throw new UserNotLoggedInException();
         }
+
+        List<Trip> tripList = new ArrayList<Trip>();
+        if (user.isFriendWith(loggedUser)) {
+            tripList = tripsByUser(user);
+        }
+        return tripList;
+
     }
 
     protected List<Trip> tripsByUser(final User user) {
